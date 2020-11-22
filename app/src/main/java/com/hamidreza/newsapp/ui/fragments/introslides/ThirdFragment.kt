1f2a3c667006e912598e3a5d4528e68667eecd83
Hamidrezakf <hrk1fard@gmail.com>
2020-11-22 18:00:45 +0330

    create singup fragment

--- app/src/main/java/com/hamidreza/newsapp/ui/fragments/introslides/ThirdFragment.kt
+++ app/src/main/java/com/hamidreza/newsapp/ui/fragments/introslides/ThirdFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hamidreza.newsapp.R
+import kotlinx.android.synthetic.main.fragment_third.*
 
 class ThirdFragment : Fragment() {
     override fun onCreateView(
@@ -16,4 +18,10 @@ class ThirdFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_third, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tv_go.setOnClickListener {
+            findNavController().navigate(R.id.action_introSliderFragment_to_signInFragment)
+        }
+    }
 }
