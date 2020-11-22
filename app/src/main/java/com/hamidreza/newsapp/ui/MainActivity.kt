65e51050de2bc21e8ef2fe71007ca0b4c2e63a1a
Hamidrezakf <hrk1fard@gmail.com>
2020-11-22 12:04:40 +0330

    add splash and intro slides fragments

---
+++ app/src/main/java/com/hamidreza/newsapp/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.hamidreza.newsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.hamidreza.newsapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottom_nav.setupWithNavController(nav_host.findNavController())
+    }
+}
---
+++ app/src/main/java/com/hamidreza/newsapp/ui/fragments/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.hamidreza.newsapp.ui.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.hamidreza.newsapp.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed(object : Runnable{
+            override fun run() {
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            }
+        },3000)
+    }
+
+}
