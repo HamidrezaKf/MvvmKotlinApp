0a4ff82934ad61fc7528b97c8310af73f8c54b33
Hamidrezakf <hrk1fard@gmail.com>
2020-12-03 11:03:36 +0330

    edit NewsViewModel.kt

--- app/src/main/java/com/hamidreza/newsapp/ui/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/hamidreza/newsapp/ui/viewmodels/NewsViewModel.kt
@@ -2,6 +2,7 @@ package com.hamidreza.newsapp.ui.viewmodels
 
 import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,27 +10,32 @@ import com.hamidreza.newsapp.data.model.remote.NewsResponse
 import com.hamidreza.newsapp.repositories.NewsRepository
 import com.hamidreza.newsapp.utils.NoInternetExceptions
 import com.hamidreza.newsapp.utils.ResultWrapper
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import retrofit2.awaitResponse
 import java.lang.Exception
 
+
 class NewsViewModel @ViewModelInject constructor(val repo : NewsRepository) :ViewModel() {
     private val TAG = "NewsViewModel"
-    val breakingNews: MutableLiveData<ResultWrapper<NewsResponse>> = MutableLiveData()
+    val _breakingNews: MutableLiveData<ResultWrapper<NewsResponse>> = MutableLiveData()
+    val breakingNews:LiveData<ResultWrapper<NewsResponse>> = _breakingNews
+
 
     fun getBreakingNews(country:String,page:Int,category:String) {
+        Log.i(TAG, "ViewModelNews: ")
         viewModelScope.launch {
             try {
                 //breakingNews.postValue(repo.getBreakingNews("us",1).await())
-                breakingNews.postValue(ResultWrapper.Loading())
+                _breakingNews.postValue(ResultWrapper.Loading())
                 val response = repo.getBreakingNews(country, page,category).await()
-                breakingNews.postValue(handleResponse(response))
+                _breakingNews.postValue(handleResponse(response))
             } catch (e: NoInternetExceptions) {
-                breakingNews.postValue(ResultWrapper.Error(e.message.toString()))
+                _breakingNews.postValue(ResultWrapper.Error(e.message.toString()))
             }catch (e: Exception){
-                breakingNews.postValue(ResultWrapper.Error(e.message.toString()))
+                _breakingNews.postValue(ResultWrapper.Error(e.message.toString()))
             }
         }
     }
